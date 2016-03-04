3975462e246476b046dad7b30079153c0676101b
Tony Shen <fengzhizi715@126.com>
2016-03-04 14:24:33 +0800

    add comment for TourDetailActivity.

--- app/src/main/java/com/magicwindow/deeplink/activity/TourDetailActivity.java
+++ app/src/main/java/com/magicwindow/deeplink/activity/TourDetailActivity.java
@@ -14,6 +14,8 @@ import cn.salesuite.saf.inject.annotation.InjectView;
 import cn.salesuite.saf.inject.annotation.OnClick;
 
 /**
+ * 旅游的详情页面,绑定了uber的mLink服务.</br>
+ * Config.MWS[91]:大众点评的魔窗位,跳转到大众点评三亚的美食服务 </br>
  * @author aaron
  * @date 16/01/14
  */
